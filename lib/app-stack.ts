045d779f97c23c2dde697149fbba215b99677167
EC2 Default User <ec2-user@ip-172-31-41-187.ap-northeast-1.compute.internal>
2022-11-02 07:10:50 +0000

    update circleci config file

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,16 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // 아래 내용을 추가합니다
+    const fn = new lambda.Function(this, 'LambdaFunction', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda/')
+    });
   }
 }
